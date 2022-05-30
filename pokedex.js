a238bb30dfb93e46736aca44553089609a574b70
Pau-Isach-Noguera <pau.nughy@gmail.com>
2022-05-30 11:48:13 +0200

    Proyecto Upgrade Pokedex JS

--- pokedex.js
+++ pokedex.js
@@ -1,8 +1,8 @@
 // <!-- UPGRADEHUB POKÉDEX - Pau Isach Noguera -->
 
-//Definimos una variable y le passamos una lista ol vacia creada en el HTML. 
+//Definimos una variable y le passamos una lista "ol" vacia creada en el HTML. 
 const pokedex = document.getElementById("pokedex");
-//Definimos una matriz vacia donde guardaremos los pokémons. Estra matriz no se modifica!
+//Definimos una matriz vacia donde guardaremos los pokémons. Esta matriz no se modifica!
 const ALL_POKEMONS = []; 
 //Definimos una matriz con los tipos de pokémons que podemos encontrar en la api. 
 const TYPES = [
@@ -26,8 +26,10 @@ const TYPES = [
   "fairy",
 ];
 
-//Definimos una función y le retornamos la dirección de la api pokemos (fetch para url).
-
+//Definimos una función asíncrona que recojerá la api de pokémons. 
+//Definimos una variable de espera y con el fetch recojemos los datos de la api. 
+//Definimos una variable de espera y con el json habilitamos su lectura. 
+//Definimos una variable de espera con los resultados finales, que retornamos. 
 const getAllPokemons = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const responseJson = await response.json();
@@ -35,8 +37,10 @@ const getAllPokemons = async () => {
   return results;
 };
 
-//Definimos una función y le retornamos una dirección vacia donde guardaremos pokemonsi (fethc para url).
-
+//Definimos una función asíncrona que recojerá la información individual de un pokémon. 
+//Definimos una variable de espera y con el fetch recojemos los datos de la api. 
+//Definimos una variable de espera y con el json habilitamos su lectura. 
+//Definimos una variable de espera con los resultados finales (nombre, id, tipo e imagen), que retornamos. 
 const getIndividualPokemon = async (url) => {
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -49,31 +53,33 @@ const getIndividualPokemon = async (url) => {
       return pokemon;
 };
 
-//Definimos una función y que va a recibir una lista de pokemons y la va a ejecutar. 
+//Definimos una función que va a recibir una lista "li" de pokémons y la va a ejecutar. 
 const drawPokemons = (pokemons) => {
     //Primero borra todos los pokémons dentro de la pokédex, siempre primer paso. 
     pokedex.innerHTML = ""; 
     //Y después irá añadiendo un pokémon por cada elemento del array que reciba. 
     pokemons.forEach((poke) => {
-    //Definimos una variable, le asignamos un listado li y le añadimos cada carta pokémon. 
+    //Definimos una variable, le asignamos un listado "li" y le añadimos cada carta pokémon. 
     const li = document.createElement("li");
     li.classList.add("card");
-    //Definimos una variable donde guardaremos tres informaciones (imagen, nombre y tipo).
+    //Definimos una variable donde guardaremos las informaciones id, imagen, nombre y tipo.
     const html = `
             <p class="card__id">#${poke.id}</p>
             <img class="card__image" src=${poke.image} alt=${poke.name}>
             <p class="card__title">${poke.name}<p>
             <div class="card__subtitle">${poke.type[0]} ${poke.type[1] ? `, ${poke.type[1]}` : ''}</div>
         `;
-    //Añadimos las informaciones a cada carta del listado li definido anteriormente. 
+    //Añadimos las informaciones a cada carta del listado "li" en el documento HTML. 
     li.innerHTML = html;
+    //Bucle if para pintar los pokémons conseguidos en verde y añadirlo al listado "li".
     if(poke.id === 1 || poke.id === 4 || poke.id === 7 || poke.id === 25){
         li.classList.add("green");
     }
+    //Bucle if para pintar los pokémons a conseguir en naranja y añadirlo al listado "li".
     if(poke.id === 54 || poke.id === 142){
         li.classList.add("orange");
     }
-    //Pasamos la lista li al listado lo que hemos definido al principio del codigo y en el HTML. 
+    //Pasamos la lista "li" al listado "lo" que hemos definido al principio del codigo y en el HTML. 
     pokedex.appendChild(li);
   });
 };
@@ -82,38 +88,39 @@ const drawPokemons = (pokemons) => {
 const filter = (event) => {
     //Definimos una variable y le asignamos que todos los valores sean LowerCase. 
     const inputValue = event.target.value.toLowerCase();
-    // //Definimos una función que retornará el filtraje de pokémon por nombre y número. 
+    //Definimos una función que retornará el filtraje de pokémon por nombre y número. 
     const filtered = ALL_POKEMONS.filter((pokemon) => {
-    // //Definimos dos variables y le asignamos sus respectivas funciones y el input para filtrar. 
-    // //Ahora ya podremos buscar los pokémons por nombre o número en el input de filtraje. 
+    //Definimos dos variables y les asignamos sus respectivas funciones y el input para filtrar. 
+    //Ahora ya podremos buscar los pokémons por nombre o número en el input de filtraje. 
     const matchName = pokemon.name.toLowerCase().includes(inputValue);
     const matchId = pokemon.id === Number(inputValue);
-    // //Retornamos los valores de nombre y número de pokémon. 
+    //Retornamos los valores de nombre y número de pokémon. 
     return matchName || matchId;
     });
-   //Llamamos la función anterior que recibia todos los pokémons pero le pasamos 
-   //el valor retornado de la función de filtraje que hemos definido. 
+//Llamamos la función anterior que recibia todos los pokémons pero le pasamos 
+//el valor retornado de la función de filtraje que hemos definido. 
 drawPokemons(filtered);
 };
 
-// //Definimos una función que añada un evento en el input para el filtraje de pokémon. 
+//Definimos una función de evento para el input y el selector de filtraje pokémon. 
 const addAllMyEventsListeners = () => {
     //El input lo cojemos del HTML y lo relacionamos con el evento para filtrar. 
+    //Igualmente, cojemos el selector del HTML y lo relacionamos con el evento. 
     document.getElementById("input-search").addEventListener("input", filter);
     document.getElementById("select-type").addEventListener("change",filterByType);
 };
 
 //Definimos una función para poder filtrar los pokemon por su tipologia. 
 const filterByType = (event) => {
-    //Definimos una constante donde le pasaremos la posició (vacia de momento) de un pokémon. 
+    //Definimos una constante donde le pasaremos la información del tipo de pokémon a filtrar. 
     const typeToFilter = event.target.value;
-    //Definimos un condicional que retornará toda la lista de pikemons si filtramos por ALL. 
+    //Definimos un condicional que retornará toda la lista de pokémons si filtramos por ALL. 
     if (typeToFilter === 'all') {
         return drawPokemons(ALL_POKEMONS);
     }
     //Definimos otra función que filtrará y retornará los pokemons según por el tipo que filtres. 
     const filtered = ALL_POKEMONS.filter((pokemon) => {
-    //Definimos una variable ddonde guardaremos los pokemons que hayamos filtrado. 
+    //Definimos una variable donde guardaremos los pokemons que hayamos filtrado segun el tipo. 
     const matchType = pokemon.type.includes(typeToFilter);
         return matchType;
     });
@@ -122,41 +129,21 @@ const filterByType = (event) => {
 drawPokemons(filtered);
 };
 
-//Definimos una función que añadirá los botones necesarios para el filtraje. 
-const drawTypesButtons = () => {
-//Definimos una variable y le asignamos el div que hemos definido en el HTML. 
-const typesContainer$$ = document.querySelector(".types");
-
-//Definimos una función con un for que creará un span por cada tipo de pokemon. 
-//Añadimos los tipos en estos divs y le asignamos con un evento que nos filtre por tipos. 
-//Añadimos el texto y también añadimos todo el bloque a la pantalla. 
-// TYPES.forEach((type) => {
-//     const span = document.createElement("span");
-//     span.classList.add(type);
-//     span.addEventListener("click", filterByType);
-//     span.innerText = type;
-//     typesContainer$$.appendChild(span);
-//   });
-};
-
-//Definimos una función asíncrona que nos va a recoger la información respetando un orden. 
+//Definimos una función asíncrona final que nos va a inicializar el código. 
 const startMyCode = async () => {
-    //Llamamos las dos sfunciones que contienen los botones y el input de filtraje. 
+    //Llamamos las dos funciones que contienen el input y el selector de filtraje. 
     addAllMyEventsListeners();
-    drawTypesButtons();
-    //Definimos una funcion con el await que espere a que recoja toda la informacion de la api. 
+    //Definimos una función de espera que recoja los datos de la api pokémon.  
     const allPokemons = await getAllPokemons(); 
-    console.log(allPokemons)
-    //Definimos un bucle for que recorra toda la lista de pokémon de la api. 
+    //Definimos un bucle for que recorra toda la lista de pokémon de la api individualmente. 
     for (const pokemon of allPokemons) {
-        //Definimos una constante con el await para que espere que recoja la informacion de cada pokémon. 
+        //Definimos una variable de espera que recoja los datos de cada pokémon a partir de la url. 
         const pokemonInfo = await getIndividualPokemon(pokemon.url);
-    //Quando termine de leer toda la informacion individual, añade todo los pokemons de golpe. 
+    //Cuando termine de leer toda la informacion individual, añade todo los pokemons de golpe. 
     ALL_POKEMONS.push(pokemonInfo);
   }
-  //Imprimir por pantalla todos los pokemons. 
-  console.log(ALL_POKEMONS);
+  //Imprimir por pantalla por consola todos los pokemons. 
   drawPokemons(ALL_POKEMONS);
 };
-//Llamamos la función anterior que imprime todos los pokémons de la api. 
+//Llamamos la función anterior que inicializa el programa de la pokédex.  
 startMyCode();
--- toDo.js
+++ toDo.js
@@ -1,14 +1,19 @@
 // <!-- UPGRADEHUB POKÉDEX - Pau Isach Noguera -->
 
-//Definimos una matriz vacia donde guardaremos los pokémons. Estra matriz no se modifica!
+//Definimos una matriz vacia donde guardaremos los pokémons. Esta matriz no se modifica!
 const ALL_POKEMONS = [];
 
+//Definimos una variable que recoja el input definido en el documento HTML. 
+//Definimos una variable que recoja el botón definido en el documento HTML. 
+//Definimos una variable que recoja la lista definido en el documento HTML. 
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ol = document.querySelector("ol");
 
-//Definimos una función y le retornamos la dirección de la api pokemos (fetch para url).
-
+//Definimos una función asíncrona que recojerá la api de pokémons. 
+//Definimos una variable de espera y con el fetch recojemos los datos de la api. 
+//Definimos una variable de espera y con el json habilitamos su lectura. 
+//Definimos una variable de espera con los resultados finales, que retornamos.
 const getAllPokemons = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const responseJson = await response.json();
@@ -16,8 +21,10 @@ const getAllPokemons = async () => {
   return results;
 };
 
-//Definimos una función y le retornamos una dirección vacia donde guardaremos pokemonsi (fethc para url).
-
+//Definimos una función asíncrona que recojerá la información individual de un pokémon. 
+//Definimos una variable de espera y con el fetch recojemos los datos de la api. 
+//Definimos una variable de espera y con el json habilitamos su lectura. 
+//Definimos una variable de espera con los resultados finales (nombre, id, tipo e imagen), que retornamos. 
 const getIndividualPokemon = async (url) => {
   const response = await fetch(url);
   const responseJson = await response.json();
@@ -30,9 +37,9 @@ const getIndividualPokemon = async (url) => {
   return pokemon;
 };
 
+//Definimos una función para poder hacer el filtraje de pokémons. 
 const filter = (event) => {
   //Definimos una variable y le asignamos que todos los valores sean LowerCase.
-  console.log(input.value);
   const inputValue = input.value.toLowerCase();
   // //Definimos una función que retornará el filtraje de pokémon por nombre y número.
   const filtered = ALL_POKEMONS.filter((pokemon) => {
@@ -45,59 +52,69 @@ const filter = (event) => {
   });
   //Llamamos la función anterior que recibia todos los pokémons pero le pasamos
   //el valor retornado de la función de filtraje que hemos definido.
-  console.log("filtered", filtered);
   creation(filtered);
 };
 
-// crear una funcion que cree 3 elementos y darle contenido del input.
-
-// <div class="card__subtitle">${poke.type} ${poke.type[1] ? `, ${poke.type[1]}` : ''}</div>
+//Definimos una función que va a recibir una lista "li" de pokémons y la va a ejecutar. 
 const creation = (pokemons) => {
-  // const li$$ = document.createElement("li")
-
-  pokemons.forEach((poke) => {
-    //Definimos una variable, le asignamos un listado li y le añadimos cada carta pokémon.
+//Definimos una función con un bucle for para que recorra todos los pokémons individualmente. 
+pokemons.forEach((poke) => {
+    //Definimos una variable para crear un div que englobe los dos botones. 
+    //Definimos dos variables para crear los dos botones, añadir y borrar. 
     const buttonsDiv = document.createElement("div");
     const button1 = document.createElement("button");
     const button2 = document.createElement("button");
+    //Definimos una variable para crear una lista "li" y le añadimos los pokemons individualmente. 
     const li = document.createElement("li");
     li.classList.add("card");
+    //Añadimos el div para los botones juntamente con los respectivos. 
     buttonsDiv.classList.add("card__buttons");
     button1.classList.add("card__buttons__delete");
     button2.classList.add("card__buttons__catch");
 
-    //Definimos una variable donde guardaremos tres informaciones (imagen, nombre y tipo).
+    //Definimos una variable donde guardaremos las informaciones id, imagen, nombre y tipo.
+    //Variable definida entre acentos para poder añadir variables de javas script dentro del string creado de HTML. 
     const html = `
                 <p class="card__id">#${poke.id}</p>
                 <img class="card__image" src=${poke.image} alt=${poke.name}>
                 <p class="card__title">${poke.name}<p>
-                <div class="card__subtitle">${poke.type[0]} ${
-      poke.type[1] ? `, ${poke.type[1]}` : ""
-    }</div>
+                <div class="card__subtitle">${poke.type[0]} ${poke.type[1] ? `, ${poke.type[1]}` : ""}</div>
             `;
     //Añadimos las informaciones a cada carta del listado li definido anteriormente.
     li.innerHTML = html;
+    //Bucle if para pintar los pokémons conseguidos en verde y añadirlo al listado "li".
     if (poke.id === 1 || poke.id === 4 || poke.id === 7 || poke.id === 25) {
       li.classList.add("green");
     }
+    //Añadir texto en el botón de borrar carta de pokemon.
+    //Definimos una función con un evento para borrar la información del pokémon. 
     button1.innerText = "X";
     const borrar = (event) => {
         event.remove();
     };
+    //Esta función y condicional son para todos los pokémons excepto los (6) por defecto.  
+    //Definimos una función con un evento de click del ratón para el botón tic/refresh.
     button1.addEventListener("click", () => borrar(li));
+    //Definimos una función para pintar un texto u otro en el botón según corresponda. 
     const catched = (event) => {
+        //Definimos un bucle for para que aparezca un "tic" si NO disponemos del pokémon.
         if(li.classList.contains("green")){
             li.classList.remove("green");
             button2.innerText = "✔";
+          //En cambio, definimos un else para que aprarezca un "refresh" si disponemos del pokémon.  
         } else {
             li.classList.add("green");
             button2.innerText = "↻";
 
         }
     };
+    //Esta función y condicional sólo son para los (6) pokemons que aprarecen por defecto. 
+    //Definimos una función con un evento de click del ratón para el botón tic/refresh.
     button2.addEventListener("click", () => catched(li));
+    //Definimos un bucle if para que aprarezca un "refresh" si disponemos del pokémon. 
     if(li.classList.contains("green")){
         button2.innerText = "↻";
+        //En cambio, definimos un else para que aparezca un "tic" si NO disponemos del pokémon. 
     } else {
         button2.innerText = "✔";
 
@@ -110,42 +127,43 @@ const creation = (pokemons) => {
   });
 };
 
-// //Definimos una función que añada un evento en el input para el filtraje de pokémon.
+//Definimos una función de evento para el botón de filtraje pokémon. 
 const addAllMyEventsListeners = () => {
-  //El input lo cojemos del HTML y lo relacionamos con el evento para filtrar.
-  // document.getElementById("input-search").addEventListener("input", filter);
-  // document.getElementById("select-type").addEventListener("change",filterByType);
-  // input$$.addEventListener("input", filter);
+  //El botón lo cojemos del HTML y lo relacionamos con el evento para filtrar. 
   button.addEventListener("click", filter);
 };
 
+//Definimos una función asíncrona final que nos va a inicializar el código. 
 const startMyCode = async () => {
+  //Definimos una variable con una array vacia para guardar los (6) pokémons por defecto. 
   const startPoint = [];
-  //Llamamos las dos sfunciones que contienen los botones y el input de filtraje.
+  //Llamamos la función que contiene el botón para el inicio de filtraje. 
   addAllMyEventsListeners();
-  //Definimos una funcion con el await que espere a que recoja toda la informacion de la api.
+  //Definimos una función de espera que recoja toda la información de la api pokémon.
   const allPokemons = await getAllPokemons();
-  console.log(allPokemons);
-  //Definimos un bucle for que recorra toda la lista de pokémon de la api.
+  //Definimos un bucle for que recorra toda la lista de pokémon de la api individualmente.
   for (const pokemon of allPokemons) {
-    //Definimos una constante con el await para que espere que recoja la informacion de cada pokémon.
+    //Definimos una variable de espera que recoja los datos de cada pokémon a partir de la url.
     const pokemonInfo = await getIndividualPokemon(pokemon.url);
-    //Quando termine de leer toda la informacion individual, añade todo los pokemons de golpe.
+    //Definimos un condicional if para que aparezcan en verde los pokémon con las id siguientes. 
     if (
       pokemonInfo.id === 1 ||
       pokemonInfo.id === 4 ||
       pokemonInfo.id === 7 ||
       pokemonInfo.id === 25
     ) {
+      //Añadimos los pokemons a la array vacia que creamos anteriormente. 
       startPoint.push(pokemonInfo);
     }
+    //Definimos un condicional if para que aparezcan en naranja los pokémon con las id siguientes. 
     if (pokemonInfo.id === 54 || pokemonInfo.id === 142) {
+      //Añadimos los pokémons a la array vacia que creamos anteriormente.
       startPoint.push(pokemonInfo);
     }
+    //Añadimos los pokémons también a la array vacia inicial (la que se imprimirá por pantalla).
     ALL_POKEMONS.push(pokemonInfo);
   }
-  //Imprimir por pantalla todos los pokemons.
-  console.log(ALL_POKEMONS);
+  //Imprimir por pantalla todos los pokémons.
   creation(startPoint);
 };
 //Llamamos la función anterior que imprime todos los pokémons de la api.
